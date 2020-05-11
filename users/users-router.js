9632829a9c39cad051d6b68013c161215b4aa235
April Ma <54913304+april5622@users.noreply.github.com>
2020-05-10 17:26:27 -0700

    MVP done

---
+++ users/users-router.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const users = require("./users-model")
+const restrict = require("../middleware/restrict")
+
+const router = express.Router()
+
+
+router.get("/", restrict(), async (req, res, next) => {
+	try {
+		res.json(await users.find())
+	} catch(err) {
+		next(err)
+	}
+})
+
+module.exports = router
